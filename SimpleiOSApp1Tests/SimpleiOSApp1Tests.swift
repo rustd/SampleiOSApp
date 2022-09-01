a096972529e82f2780c32644c5b3ce3ffa955115
Pranav Rastogi <rustd@users.noreply.github.com>
2022-09-01 16:50:30 -0700

    Updating paths

---
+++ SimpleiOSApp1Tests/SimpleiOSApp1Tests.swift
@@ -0,0 +1,54 @@
+//
+//  SimpleiOSApp1Tests.swift
+//  SimpleiOSApp1Tests
+//
+//  Created by Pranav Rastogi on 9/1/22.
+//
+
+import XCTest
+@testable import SimpleiOSApp1
+
+class SimpleiOSApp1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample1() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        
+        // XCTAssert(false)
+    }
+    
+    
+    func testExample2() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample1() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testPerformanceExample2() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
